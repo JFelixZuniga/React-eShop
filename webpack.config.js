853bd7009fab48cf370c575100f372da1de59005
JFelixZuniga <johnfzuniga@gmail.com>
2021-10-21 09:06:15 -0300

    Páginas de Login y Recuperar contraseña | Add React router

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    publicPath: "/",
   },
   mode: "development",
   resolve: {
@@ -30,7 +31,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.(css|scss)$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
@@ -48,5 +49,6 @@ module.exports = {
     allowedHosts: path.join(__dirname, "dist"),
     port: 3000,
     compress: true,
+    historyApiFallback: true,
   },
 };
