850a61799befd09786a7e79a98bc175c63c16648
JFelixZuniga <johnfzuniga@gmail.com>
2021-10-25 12:11:10 -0300

    Páginas restantes/assets ADD

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import "@styles/NotFound.scss";
+
+import pageNotFound from "@icons/page_not_found.svg";
 
 const NotFound = () => {
-	return (
-		<div>
-			No encontrado, error 404.
-		</div>
-	);
-}
+  return (
+    <div className="container-image">
+      <img
+        src={pageNotFound}
+        alt="Página no encontrada"
+        className="not-found"
+      />
+    </div>
+  );
+};
 
 export default NotFound;
